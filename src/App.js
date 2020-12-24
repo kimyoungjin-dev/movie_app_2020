32462420fc280a408a4a9c0f70abfd61960ec9dc
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-24 21:05:49 +0900

    Add router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import About from "./routes/About";
 import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
-
 function App() {
   return (
     <HashRouter>
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
